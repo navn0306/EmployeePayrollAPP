328fa7c3f460bc2a83f489a7c785aaf98b28b9e1
Navneet <navn0306@gmail.com>
2022-01-28 00:23:47 +0530

    ADDED: Perform Validation Employee Payroll Data setter methods.

--- JS/payroll_form.js
+++ JS/payroll_form.js
@@ -54,8 +54,17 @@ class EmployeePayrollData {
     get startDate() {
         return this._startDate;
     }
+    
     set startDate(startDate) {
+        let currentDate = new Date();
+        if (startDate > currentDate) {
+            throw "Start date not a future date";
+        }
 
+        var diff = Math.abs(currentDate.getTime - startDate.getTime());
+        if (diff / (1000 * 60 * 24) > 30) {
+            throw "start date is beyond 30 days";
+        }
         this._startDate = startDate;
     }
 
--- JS/validation.js
+++ JS/validation.js
@@ -1,8 +1,21 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    validateName();
     validateSalary();
 });
 
-//UseCase 8
+function validateName() {
+    const name = document.querySelector('#name1');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        const nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if (nameRegex.test(name.value)) {
+            textError.textContent = "";
+        }
+        else {
+            textError.textContent = "Name is invalid";
+        }
+    });
+}
 function validateSalary() {
     const salary = document.querySelector("#salary");
     const output = document.querySelector('.salary-output');
